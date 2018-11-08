a9bfcfc893134b306be302dd9435e6050ffc6b80
vinutama <muhammadkhevin@gmail.com>
2018-11-08 19:00:21 +0700

    multiple done

--- index.js
+++ index.js
@@ -1,9 +1,49 @@
 const fs = require('fs');
-const sleep = require('sleep');
+// const sleep = require('sleep');
+//npm error bang gatau kenapa :(
 
-function match_data(parent_file, children_file) {
-  // Code here
+
+function sleep(milliseconds) {
+  var start = new Date().getTime();
+  for (var i = 0; i < 1e7; i++) {
+    if ((new Date().getTime() - start) > milliseconds) {
+      break;
+    }
+  }
 }
 
-match_data('./parents.json', './children.json')
-console.log("Notification : Data sedang diproses !");
+function match_data(parent_file, children_file, cb) {
+  fs.readFile(parent_file, 'utf8', function (err, data) {
+    sleep(5000)
+    if (err) console.log(err)
+    else {
+      let parent_data = JSON.parse(data)
+      fs.readFile(children_file, 'utf8', function (err, data) {
+        if (err) console.log(err)
+        else {
+          let child_data = JSON.parse(data)
+          child_data.forEach(function (child) {
+            let childFamily = child.family
+            parent_data.forEach(function (parent) {
+              if (childFamily === parent.last_name) {
+                if (!parent.children) {
+                  parent.children = [child.full_name]
+                }
+                if (parent.children[0] != child.full_name) {
+                  parent.children.push(child.full_name)
+                }
+              }
+            });
+          });
+        }
+        cb(parent_data)
+      })
+    }
+  })
+}
+match_data('./parents.json', './children.json', function (data) {
+  console.log(data)
+})
+console.log(`Data Sedang Di Proses!!`);
+
+
