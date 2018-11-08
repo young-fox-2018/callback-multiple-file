87f3b447a780f36dfcd12e6c7068a1090a9e5f2a
Zhang <zhang@android-42520aa2424d51bc.localdomain>
2018-11-08 18:11:09 +0700

    done

--- index.js
+++ index.js
@@ -1,9 +1,49 @@
 const fs = require('fs');
-const sleep = require('sleep');
+// const sleep = require('sleep');
 
-function match_data(parent_file, children_file) {
-  // Code here
+//sleep
+function sleep (milliseconds) {
+  var start = new Date().getTime();
+  for (var i = 0; i < 1e7; i++) {
+    if ((new Date().getTime() - start) > milliseconds) {
+      break;
+    }
+  }
 }
 
-match_data('./parents.json', './children.json')
+function match_data(parent_file, children_file) {
+
+  fs.readFile(parent_file , function(err, dataParent){
+    if (!err){
+      let parent = JSON.parse(dataParent)
+      console.log('Generate parent')
+      sleep(2000)
+      fs.readFile(children_file , function(err, dataChildren){
+        if (!err){
+          //buat ngecek
+          //console.log(JSON.parse(data))
+          let child = JSON.parse(dataChildren)
+
+          for (let i = 0; i < parent.length; i++) {
+            parent[i].children = []       
+            
+            for (let j = 0; j < child.length; j++) {
+              if(child[j].family === parent[i].last_name){
+                parent[i].children.push(child[j].full_name)
+              }
+            }
+          }
+        }
+        console.log(parent)
+      })
+    }
+  })
+
+
+
+}
+
+match_data('/Users/zhang/phase1/p1w2/callback-multiple-file/parents.json', '/Users/zhang/phase1/p1w2/callback-multiple-file/children.json')
 console.log("Notification : Data sedang diproses !");
+
+
