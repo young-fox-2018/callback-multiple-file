2532549383a4597a9653c26568cefa62e014f80f
javasusanto <herisusanto.id@gmail.com>
2018-11-08 19:33:50 +0700

    Callback Multiple File

--- index.js
+++ index.js
@@ -1,9 +1,55 @@
-const fs = require('fs');
-const sleep = require('sleep');
+const fs = require('fs')
+// const sleep = require('sleep')
+// const pathParent = 'parents.json'
 
-function match_data(parent_file, children_file) {
-  // Code here
+
+function readData(path, callback) {
+  fs.readFile(path, 'utf8', (err, data) => {
+    if (err) callback(err, null)
+    else callback(null, data)
+  })
 }
 
-match_data('./parents.json', './children.json')
+function save(path, callback) {
+  fs.writeFile(path, (err, data) => {
+    if (err) callback(err, null)
+    else callback(`Data berhasil disimpan!`)
+  })
+}
+
+function match_data(parent_file, children_file, callback) {
+  // Code here
+  fs.readFile(parent_file, (err, data) => {
+    if (err) {
+      callback(err, null)
+    } else {
+      let parent_data = JSON.parse(data)
+      fs.readFile(children_file, (err, data) => {
+        if (err) {
+          callback(err, null)
+        } else {
+          let children_data = JSON.parse(data)
+          parent_data.forEach(parent => {
+            parent.children = []
+            children_data.forEach(children => {
+              if (parent.last_name === children.family) {
+                parent.children.push(children.full_name)
+              }
+            })
+          })
+          callback(null, parent_data)
+        }
+      })
+
+    }
+  })
+
+}
+
+match_data('./parents.json', './children.json', (err, data) => {
+  if (err) throw err
+  else {
+    console.log(data)
+  }
+})
 console.log("Notification : Data sedang diproses !");
