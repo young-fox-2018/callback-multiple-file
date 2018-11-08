bbb33e80596c08cd7c8dcc494af619abf8175ac0
Ribka Haryuni <ribka.blue@gmail.com>
2018-11-08 23:08:30 +0700

    submit file callback

--- index.js
+++ index.js
@@ -1,9 +1,48 @@
 const fs = require('fs');
 const sleep = require('sleep');
 
-function match_data(parent_file, children_file) {
-  // Code here
+function readFile(path, cb){
+  fs.readFile(path, 'utf8', function(err, data){
+    if(err){
+      sleep.sleep(2)
+      cb(err, null)
+    }else{
+      sleep.sleep(2)
+      cb(null, data)
+    }
+  })
 }
 
-match_data('./parents.json', './children.json')
+function match_data(parent_file, children_file, cb) {
+  readFile(parent_file, (err, data) => {
+    if(err) cb(err)
+    else{
+      let parentList = JSON.parse(data)
+      readFile(children_file, (err, data) => {
+        if(err) cb(err)
+        else{
+          let dataChildren = JSON.parse(data)
+          parentList.forEach(item => {
+            dataChildren.forEach(element => {
+              if(item.last_name === element.family){
+                if(item.children === undefined){
+                  item.children = [element.full_name]
+                }else{
+                  item.children.push(element.full_name)
+                }
+              }
+            })
+          })
+          cb(parentList)
+        }
+      })
+    }
+  })
+}
+
+function display(value){
+  console.log(value)
+}
+
+match_data('./parents.json', './children.json', display)
 console.log("Notification : Data sedang diproses !");
