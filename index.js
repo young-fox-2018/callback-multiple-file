d1d2769cedcddfc80753802bb25ba8c2eec08938
patriagani <patria.gani@gmail.com>
2018-11-08 19:14:13 +0700

    callback read multiple file done

--- index.js
+++ index.js
@@ -1,9 +1,25 @@
 const fs = require('fs');
-const sleep = require('sleep');
+// const sleep = require('sleep');
 
-function match_data(parent_file, children_file) {
-  // Code here
+function match_data(parent_file, children_file, cb) {
+  fs.readFile(parent_file, function(err, data){
+    let parent_data = JSON.parse(data)
+    fs.readFile(children_file, function(err, data){
+      let children_data = JSON.parse(data)
+      for (let i = 0; i < parent_data.length; i++) {
+        parent_data[i].children = []
+        for (let j = 0; j < children_data.length; j++) {
+          if (children_data[j].family === parent_data[i].last_name) {
+            parent_data[i].children.push(children_data[j].full_name)
+          }
+        }
+      }
+      cb(parent_data)
+    })
+  })
 }
 
-match_data('./parents.json', './children.json')
+match_data('./parents.json', './children.json',function(data){
+  console.log(data);
+})
 console.log("Notification : Data sedang diproses !");
