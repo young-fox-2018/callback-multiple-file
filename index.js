201c78d67dd9dca6e5a6899765c7922302d72acd
fauzia <fauziaazia171097@gmail.com>
2018-11-08 18:38:42 +0700

    multiple-file

--- index.js
+++ index.js
@@ -1,7 +1,44 @@
 const fs = require('fs');
-const sleep = require('sleep');
-
+// const sleep = require('sleep');
+function sleep (milliseconds) {
+  var start = new Date().getTime();
+  for (var i = 0; i < 1e7; i++) {
+    if ((new Date().getTime() - start) > milliseconds) {
+      break;
+    }
+  }
+}
 function match_data(parent_file, children_file) {
+  fs.readFile(parent_file, function(err, data) {
+    if (err) {
+        throw err;
+    }else{
+
+      let parent = JSON.parse(data);
+      // console.log(parent)
+      
+      fs.readFile(children_file, function(err, data) {
+        if (err) {
+            throw err;
+        }
+        let children = JSON.parse(data)
+        // console.log(children)
+        for(let i = 0 ; i < parent.length;i++){
+          for(let j = 0 ; j < children.length; j++){
+            if(children[j].family === parent[i].last_name){
+              if(parent[i].children === undefined){
+                parent[i].children = []
+              }
+              parent[i].children.push(children[j].full_name)
+            }
+          }
+        }
+        console.log(parent)
+    });
+
+
+    }
+});
   // Code here
 }
 
