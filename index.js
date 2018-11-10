019decd1353287916b72a9e24b0ca7630decfefd
Desy Armariena <desy.armariena@gmail.com>
2018-11-10 22:00:47 +0700

    Refactor, readData into separate function

--- index.js
+++ index.js
@@ -1,40 +1,51 @@
 const fs = require('fs');
 const sleep = require('sleep');
 
-function match_data(parent_file, children_file) {
+function match_data(parent_file, children_file, cb) {
   // Code here
-  fs.readFile(parent_file, function(err, data) {
-    if(err) console.log(err)
-    
-    console.log(`Extracting parent data...`);
-    sleep.sleep(2);
-    let parent_data = JSON.parse(data);
-    
-    fs.readFile(children_file, function(err, data) {
-      if(err) console.log(err)
-      
-      console.log(`Extracting children data...`);
+  readData(parent_file, function(err, data_parent) {
+    if (err) {
+      cb(err)
+    } else {
+      console.log(`Extracting parent data...`);
       sleep.sleep(2);
-      let children = JSON.parse(data);
 
-      //loop data parent
-      for (let i = 0; i < parent_data.length; i++) {
-        //loop data children, samakan family name children dengan last name parent
-        for (let j = 0; j < children.length; j++) {
-          if (parent_data[i].last_name === children[j].family) {
-            if (parent_data[i].children === undefined) {
-              parent_data[i].children = [children[j].full_name];
-            } else {
-              parent_data[i].children.push(children[j].full_name);
+      readData(children_file, function(err, data_children) {
+        if (err) {
+          cb (err)
+        } else {
+          console.log(`Extracting children data...`);
+          sleep.sleep(2);
+
+          for (let i = 0; i < data_parent.length; i++) {
+            //loop data children, samakan family name children dengan last name parent
+            for (let j = 0; j < data_children.length; j++) {
+              if (data_parent[i].last_name === data_children[j].family) {
+                if (data_parent[i].children === undefined) {
+                  data_parent[i].children = [data_children[j].full_name];
+                } else {
+                  data_parent[i].children.push(data_children[j].full_name);
+                }
+              }
             }
           }
+          console.log(data_parent);
         }
-      }
-      console.log(parent_data);
-      
-    });
-  });
+      })
+    }
+  })
 }
 
-match_data('./parents.json', './children.json')
+function readData(file, callback) {
+  fs.readFile(file, 'utf8', function(err, data) {
+    if (err) {
+      callback(err)
+    } else {
+      callback(null, JSON.parse(data));
+    }
+  })
+
+}
+
+match_data('./parents.json', './children.json');
 console.log("Notification : Data sedang diproses !");
